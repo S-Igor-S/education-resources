211634d3ae74845a957e4fe4660309ef9f14c7e1
Ihor Prachuk <ihor.prachuk@nixs.com>
2024-03-14 10:09:58 +0100

    Revert "Merge pull request #3 from S-Igor-S/revert-2-feature/long_polling_#3"
    This reverts commit d9f2eb0d0522a3ffe0627a5e22290e608307c17f, reversing
    changes made to 2ffc98b736663fb46186f35fbc53fa3ce2c6a3c5.

---
+++ config/bot.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    "/start" => [
+        "success" => "Hello {usernames}. I can help you to save useful info for skill-up",
+        "wrong_message_format" => "{usernames}. Please, use next format for adding message to resources list: \"/save YOUR_URL\"",
+    ],
+    "/save" => [
+        "success" => "Thanks {usernames}. Your resource was saved",
+        "wrong_message_format" => "{usernames}. Please, use next format for adding message to resources list: \"/save YOUR_URL\"",
+    ],
+];
